fe7b6fd7dd5f8e734dc6f199931d6515737a6d90
Nguyen Quang Ha <nguyenquangha295@gmail.com>
2022-11-22 14:48:16 +0700

    update productDetails

--- pages/productDetails/index.vue
+++ pages/productDetails/index.vue
@@ -1,7 +1,48 @@
 <template>
     <div class="header-product-details-container">
-        <div class="header-product-details">
-            CPU Intel Core i5-12600K (3.7GHz turbo 4.9Ghz |10 nhân 16 luồng | 20MB Cache | 125W)
+        <div class="header-product">
+            Laptop Gigabyte Gaming G5 MD 51S1123SO ( i5-11400H/ 16GB/ 512GB SSD/ 15.6" FHD/ RTX3050Ti 4Gb/ Win11)
+        </div>
+        <div class="product-details">
+            <div class="row">
+                <div class="col-md-12 col-lg-5 col-xl-5">
+                    <div class="img-product">
+                        <img class="img-product-details" src="../../assets/img/250-21734-laptop-gigabyte-gaming-g5-md-51s1123so.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-md-12 col-lg-7 col-xl-7">
+                    <div class="test-2">
+                        <div class="product-general">
+                            Thông số sản phẩm
+                        </div>
+                        <div class="product-details-content">
+                            <li>CPU: Intel Core i5 11400H</li>
+                        </div>
+                        <div class="product-details-content">
+                            <li>RAM: 16GB</li>
+                        </div>
+                        <div class="product-details-content">
+                            <li>Ổ cứng: 512GB SSD</li>
+                        </div>
+                        <div class="product-details-content">
+                            <li>VGA: NVIDIA RTX3050Ti 4G</li>
+                        </div>
+                        <div class="product-details-content">
+                            <li>Màn hình: 15.6 inch FHD 144Hz</li>
+                        </div>
+                        <div class="product-details-content">
+                            <li>Bàn phím: có đèn led</li>
+                        </div>
+                        <div class="product-details-content">
+                            <li>HĐH: Win 11</li>
+                        </div>
+                        <div class="product-details-content">
+                            <li>Màu: Đen</li>
+                        </div>
+                        <div class=""></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -14,5 +55,41 @@ export default {
 </script>
 
 <style>
+.header-product-details-container {
+    width: 100%;
+    height: 2000px;
+    padding: 0 5%;
+}
 
+.header-product{
+    font-size: 23px;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+    display: flex;
+
+}
+
+.product-general{
+    font-size: 15px;
+    font-weight: bold;
+}
+
+.product-details{
+    width: 100%;
+    padding: 0 8%;
+}
+.img-product{
+    width: 100%;
+    height: 290px;
+}
+
+.img-product-details{
+    width: 100%;
+    height: 290px;
+}
+.test-2{
+    width: 100%;
+    height: 200px;
+}
 </style>
