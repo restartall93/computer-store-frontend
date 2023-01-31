3de11a459bbb417771de26f60ff0c9fefe87fe15
Nguyen Quang Ha <nguyenquangha295@gmail.com>
2023-01-31 11:10:51 +0700

    Update index.vue

--- pages/adminProduct/index.vue
+++ pages/adminProduct/index.vue
@@ -1,19 +1,27 @@
 <template>
     <div>
-        <div class="w-100 d-flex justify-content-end">
+        <div class="w-100 d-flex justify-content-between">
+            <div style="display: flex; align-items: center;">
+                Total: {{ total }} Products, Page:
+                <div v-for="(room, index) in totalPage">
+                    <div class="item-page" :id="'page-' + (index)" @click="chooesPage(index)">
+                        {{ index + 1 }}
+                    </div>
+                </div>
+            </div>
             <NuxtLink class="add-product d-flex mr-3 text-light p-3 bg-primary "
                 :to="{ path: 'adminProduct/CreateEditProduct', query: { 'productId': 0 } }">
                 <div class=""><font-awesome-icon icon="fa-solid fa-plus" /> </div>
-                <div class="add-product-content ml-2">Thêm mới sản phẩm</div>
+                <div class="add-product-content ml-2">Thêm SP</div>
             </NuxtLink>
         </div>
         <div class="table-responsive">
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">Mã Sản Phẩm</th>
-                        <th scope="col">Tên Sản Phẩm</th>
-                        <th scope="col">Loại Sản Phẩm</th>
+                        <th scope="col">Mã SP</th>
+                        <th scope="col">Tên SP</th>
+                        <th scope="col">Loại SP</th>
                         <th scope="col">Giá Tiền</th>
                         <th scope="col">Ảnh</th>
                         <th scope="col">Mô tả</th>
@@ -63,23 +71,40 @@
 export default {
     layout: "adminLayout",
     async beforeMount() {
+        this.currentPage = 1
         this.getProductList()
     },
     data() {
         return {
             productList: [],
+            total: 0,
+            totalPage: 0,
+            currentPage: 0,
         };
     },
     methods: {
         async getProductList() {
 
-            var response = await fetch('https://localhost:7029/api/Product/GetProducts')
+            var response = await fetch('https://localhost:7029/api/Product/GetProducts?limit=6&page=' + this.currentPage)
                 .then(res => {
                     return res.json()
                 })
-            this.productList = response
+            this.productList = response.productList
+            this.total = response.total
+            this.totalPage = response.totalPage
             console.log(JSON.stringify(this.productList))
-        }
+        },
+
+        chooesPage(index) {
+            if (this.currentPage != 0) {
+                var element = document.getElementById("page-" + (this.currentPage - 1));
+                element.classList.remove("page-item-choose");
+            }
+            var element = document.getElementById("page-" + index);
+            element.classList.add("page-item-choose");
+            this.currentPage = index + 1
+            this.getProductList()
+        },
     },
 }
 </script>
@@ -87,5 +112,22 @@ export default {
 <style>
 .add-product:hover {
     text-decoration: none;
+}
+
+.item-page {
+    height: 30px;
+    width: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 15px;
+    border: 1px solid #CCC;
+    margin-left: 3px;
+    cursor: pointer;
+}
+
+.page-item-choose {
+    color: #321FDB;
+    border: 1px solid #321FDB;
 }
 </style>
